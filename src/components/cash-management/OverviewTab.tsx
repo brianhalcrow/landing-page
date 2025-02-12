3f6ecd9c1339ba563977ac64a93403c6334a8b55
brianhalcrow <brian@sensefx.io>
2025-02-12 15:48:23 +0000

    Update OverviewTab.tsx

--- src/components/cash-management/OverviewTab.tsx
+++ src/components/cash-management/OverviewTab.tsx
@@ -1,3 +1,7 @@
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+
 const OverviewTab = () => {
   const columnDefs = useMemo(() => createColumnDefs(), []);
   const [loading, setLoading] = useState(true);
