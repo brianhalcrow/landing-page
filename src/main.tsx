666dd9bd6e97da90f78e1690b4f2df2cd0522fd1
brianhalcrow <brian@sensefx.io>
2025-02-12 18:12:16 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ModuleRegistry, ClientSideRowModelModule, RowGroupingModule, EnterpriseCoreModule } from 'ag-grid-enterprise';
 import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from './components/ThemeProvider'
+
+// Register AG Grid Enterprise modules
+ModuleRegistry.registerModules([
+  ClientSideRowModelModule,
+  RowGroupingModule,
+  EnterpriseCoreModule
+]);
 
 const queryClient = new QueryClient({
   defaultOptions: {
